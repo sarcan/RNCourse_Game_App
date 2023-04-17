4909517625a637bd9ec8be535f3869b220d3a6f1
sarcan <sarmayca@protonmail.ch>
2023-04-17 17:17:50 +0200

    initialized project

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,12 @@
+import { TextInputComponent, View } from "react-native";
+
+function StartGameScreen() {
+    return (
+        <View>
+            <TextInputComponent />
+            {/* BUTTONS */}
+        </View>
+    );
+}
+
+export default StartGameScreen;
